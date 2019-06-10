694411fad7783b23794f10d8b8f52559a1c1af97
michaeldai1006 <dtc74110@gmail.com>
2019-06-10 15:56:15 -0700

    Control item selected demo

--- TDSwiftSegmentedControl/ViewController.swift
+++ TDSwiftSegmentedControl/ViewController.swift
@@ -1,14 +1,23 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, TDSwiftSegmentedControlDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Control instance
         let controlFrame = CGRect(x: 26.0, y: 100.0, width: self.view.frame.width - 52.0, height: 40.0)
         let segmentedControl = TDSwiftSegmentedControl(frame: controlFrame,
                                                        itemTitles: ["Apple", "Banana", "Orange"])
+        
+        // Control delegate
+        segmentedControl.delegate = self
+        
+        // Add control as subview
         self.view.addSubview(segmentedControl)
+    }
+    
+    func itemSelected(atIndex index: Int) {
+        print("Item selected at index \(index)")
     }
 }
 
